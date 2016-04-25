bb0a07f4838f4432b6fe78e06dffd7e3a83fb422
Alfred-G <forever_antares@qq.com>
2016-04-25 12:36:11 +0800

    Update wine.py

--- wine.py
+++ wine.py
@@ -3,12 +3,12 @@ import pandas as pd
 from sklearn import tree
 
 df=pd.read_csv(os.getcwd()+'\wine.txt',dtype='float64',sep=',',names=list(range(14)))
-n=0
+ac=0
 for i in range(10):
     dft=df.sample(int(df.shape[0]*0.7))
     dfe=(df[~df.index.isin(dft.index)])
     model = tree.DecisionTreeClassifier(criterion='gini')
     model.fit(dft.iloc[:,1:14], dft[0])
     predicted= model.predict(dfe.iloc[:,1:14])
-    n+=sum(predicted==dfe.iloc[:,0])/len(predicted)
-print (n/10)
+    ac+=sum(predicted==dfe.iloc[:,0])/len(predicted)
+print (ac/10)
